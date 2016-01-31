5bbcc2de3f1d49088d4289691bc6a9dd4bfda1d9
Eduard Urbach <e.urbach@gmail.com>
2016-01-31 19:42:09 +0900

    Improved build script

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
+
 # Chrome
 REPO=`basename "$PWD"`
 cd ..
-zip -r $REPO/arn-chrome.zip $REPO -x *.git*
+zip -r $REPO/arn-chrome.zip $REPO -x *.git* -x *.zip* -x *.xpi* -x *.sh
 cd $REPO
 
 # Firefox
 mv manifest.json manifest-chrome.json
 cp manifest-firefox.json manifest.json
-zip -r arn-firefox.xpi ./ -x *.git*
+zip -r arn-firefox.xpi ./ -x *.git* -x *.zip* -x *.xpi* -x *.sh
 rm manifest.json
 mv manifest-chrome.json manifest.json
